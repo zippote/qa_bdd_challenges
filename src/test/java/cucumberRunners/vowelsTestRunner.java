98e715137adab67e4ebfcde7b998da3cfcb4a6db
Tino-Primer <alvaro@primer.io>
2022-12-15 15:35:50 +0100

    Updating bdd tests structure

--- src/test/java/cucumberRunners/vowelsTestRunner.java
+++ src/test/java/cucumberRunners/vowelsTestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/main/java/features",
+        features = "src/test/java/features",
         glue={"stepsDefinition"},
         monochrome = true,
         plugin = {"pretty", "json:target/JSONReports/BDD_Report.json"}

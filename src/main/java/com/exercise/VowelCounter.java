1d40a3840c5138340ea7d193727349de10ebedb3
Tino-Primer <alvaro@primer.io>
2022-12-15 03:08:53 +0100

    Adding Consonant Counter method and updating unit tests to reflect these changes

--- src/main/java/com/exercise/VowelCounter.java
+++ src/main/java/com/exercise/VowelCounter.java
@@ -6,20 +6,26 @@ import java.util.List;
 
 public class VowelCounter {
 
+//    some minor naming changes needed here for variables and methods
     public List<CountResult> getVowelListWithCount(String[] args) {
         if (args.length >= 4) {
             System.out.println("Extra arguments passed.");
             return null;
         } else {
             List<String> argumentList = Arrays.asList(args);
-            List<CountResult> vowelCount = new ArrayList<>();
-            argumentList.forEach(argument -> vowelCount.add(new CountResult(argument, getVowelCount(argument))));
-            return vowelCount;
+            List<CountResult> listCount = new ArrayList<>();
+            argumentList.forEach(argument -> listCount.add(new CountResult(argument, getVowelCount(argument), getConsonantCount(argument))));
+            return listCount;
         }
     }
 
     private Integer getVowelCount(String input) {
         String[] inputChars = input.split("");
         return (int) Arrays.stream(inputChars).filter(inputChar -> inputChar.matches("[aeiou]")).count();
+    }
+
+    private Integer getConsonantCount(String input) {
+        String[] inputChars = input.split("");
+        return (int) Arrays.stream(inputChars).filter(inputChar -> inputChar.matches("[bcdfghjklmnpqrstvwxyz]")).count();
     }
 }
--- src/test/java/com/exercise/VowelCounterTest.java
+++ src/test/java/com/exercise/VowelCounterTest.java
@@ -7,28 +7,34 @@ import java.util.List;
 
 class VowelCounterTest {
     @Test
-    void inputWordsWithVowels(){
-        VowelCounter vowelCounter = new VowelCounter();
-        List<String> expectedList = Arrays.asList("CountResult{word='casa', vowelCount=2}", "CountResult{word='amarillo', vowelCount=4}");
-        Assertions.assertEquals(expectedList.toString(), (vowelCounter.getVowelListWithCount(new String[]{"casa", "amarillo"})).toString());
+    void inputWordsWithVowelsAndConsonants(){
+        VowelCounter counter = new VowelCounter();
+        List<String> expectedList = Arrays.asList("CountResult{word='cases', vowelCount=2, consonantCount=3}", "CountResult{word='testing', vowelCount=2, consonantCount=5}");
+        Assertions.assertEquals(expectedList.toString(), (counter.getVowelListWithCount(new String[]{"cases", "testing"})).toString());
     }
     @Test
     void inputWordWithZeroVowels(){
-        VowelCounter vowelCounter = new VowelCounter();
-        List<String> expectedList = Arrays.asList("CountResult{word='bmw', vowelCount=0}");
-        Assertions.assertEquals(expectedList.toString(), (vowelCounter.getVowelListWithCount(new String[]{"bmw"})).toString());
+        VowelCounter counter = new VowelCounter();
+        List<String> expectedList = Arrays.asList("CountResult{word='bmw', vowelCount=0, consonantCount=3}");
+        Assertions.assertEquals(expectedList.toString(), (counter.getVowelListWithCount(new String[]{"bmw"})).toString());
+    }
+    @Test
+    void inputWordWithZeroConsonants(){
+        VowelCounter counter = new VowelCounter();
+        List<String> expectedList = Arrays.asList("CountResult{word='ie', vowelCount=2, consonantCount=0}");
+        Assertions.assertEquals(expectedList.toString(), (counter.getVowelListWithCount(new String[]{"ie"})).toString());
     }
 
     @Test
     void inputMoreThanFourWords(){
-        VowelCounter vowelCounter = new VowelCounter();
-        Assertions.assertNull(vowelCounter.getVowelListWithCount(new String[]{"house", "bird", "shoe", "testing", "qa" }));
+        VowelCounter counter = new VowelCounter();
+        Assertions.assertNull(counter.getVowelListWithCount(new String[]{"house", "bird", "shoe", "testing", "qa" }));
     }
 
     @Test
     void inputEmptyList(){
-        VowelCounter vowelCounter = new VowelCounter();
-        Assertions.assertEquals("[]", (vowelCounter.getVowelListWithCount(new String[]{})).toString());
+        VowelCounter counter = new VowelCounter();
+        Assertions.assertEquals("[]", (counter.getVowelListWithCount(new String[]{})).toString());
     }
 
 }

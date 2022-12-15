5d1d826d8b4f6bd0a433b8a8442da51524db2754
Tino-Primer <alvaro@primer.io>
2022-12-15 13:15:35 +0100

    Adding initial bdd tests structure

---
+++ src/main/java/features/vowelsTest.feature
@@ -0,0 +1,5 @@
+Feature: Counting vowels from string
+  Scenario: the program will count the vowels from any given string
+    Given a valid list of strings
+    When user enters a valid string
+    Then the program will display the total number of vowels in the string
---
+++ src/test/java/cucumberRunners/vowelsTestRunner.java
@@ -0,0 +1,15 @@
+package cucumberRunners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/main/java/features",
+        glue={"stepsDefinition"},
+        monochrome = true,
+        plugin = {"pretty", "json:target/JSONReports/BDD_Report.json"}
+)
+public class vowelsTestRunner {
+}
